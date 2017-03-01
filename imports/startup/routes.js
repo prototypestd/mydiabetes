7e9b81eac7766344e217f5b25e3c40d6a0e704bf
Haikal Izzuddin <heywhite69@gmail.com>
2017-03-01 19:05:57 +0800

    Break routes into another file
    * This is good for readability

---
+++ imports/startup/routes.js
@@ -0,0 +1,23 @@
+// Handles the setup of the routes within the app.
+
+// Sets the root the the 'body' tag
+BlazeLayout.setRoot('body');
+
+// Sets the index to automatically render either dashboard or the main page depending on logged in status
+FlowRouter.route('/', {
+  name: 'App_Content',
+  action() {
+	  if(Meteor.user() == undefined){
+		BlazeLayout.render('content', {main: 'dashboard'});
+	  }else{
+		BlazeLayout.render('content', {main: 'index'});
+	  }
+  }
+});
+
+// Sets up the 404 page
+FlowRouter.notFound = {
+  action() {
+    BlazeLayout.render('content', {main: 'App_notFound'});
+  }
+};
