d15dab28c10a7ab850c8569e9e966147f77e0126
Haikal Izzuddin <heywhite69@gmail.com>
2017-03-22 23:59:17 +0800

    Calculator: Improve calculation by using values set by user

--- imports/api/Calculator.js
+++ imports/api/Calculator.js
@@ -1,56 +1,81 @@
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
+import { UserInfo } from '/lib/collections';
 
 Meteor.methods({
   // Calculate the ICR (Insulin carbohydrate ratio) for patient
   // totalDose = total dose;
-  /* Todo: totalDose should be taken directly from database */
   'calculator.calcICR'( totalDose ) {
-		var ICR = 450 / totalDose;
-		var r = Math.round(ICR * 100)/100;
+		
+		const dose = UserInfo.find({
+					$or: [
+							{ userId: this.userId }
+						],
+					}).map(function(post) { return post.totalDose; });
+	  
+		const ICR = 450 / dose;
+		const r = Math.round(ICR * 100)/100;
 		return r;
   },
   // Calculate the ISF (Insulin sensitivity factor) for patient
   // totalDose = total dose;
-  /* Todo: totalDose should be taken directly from database */
   'calculator.calcISF'( totalDose ) {
-	  var ISF = 100 / totalDose;
-	  var r = Math.round(ISF * 100)/100;
+	  
+		const dose = UserInfo.find({
+					$or: [
+							{ userId: this.userId }
+						],
+					}).map(function(post) { return post.totalDose; });
+	  
+		const ISF = 100 / dose;
+		const r = Math.round(ISF * 100)/100;
 		
 	  return r;
   },
   // Calculate the correction for patient
   // reading = glucose reading; totalDose = total dose;
-  /* Todo: totalDose should be taken directly from database */
   'calculator.calcCorrection'( reading, totalDose ) {
-	  var ISF = 100 / totalDose;
-	  var correction = (reading - 6) / (Math.round(ISF * 100)/100);
-	  var r = Math.round(correction * 100)/100;
+	  
+		const dose = UserInfo.find({
+					$or: [
+							{ userId: this.userId }
+						],
+					}).map(function(post) { return post.totalDose; });
+	  
+		const ISF = 100 / dose;
+		const correction = (reading - 6) / (Math.round(ISF * 100)/100);
+		const r = Math.round(correction * 100)/100;
 	  
 	  return r;
   },
   // Calculate the dose of patient
   // carb = intake carbohydrate; reading = glucose reading; totalDose = total dose;
-  /* Todo: totalDose should be taken directly from database */
   'calculation.calcDose' ( carb, reading, totalDose ) {
-	  var ICR = 450 / totalDose;
-	  var r1 = Math.round(ICR * 100)/100;
-	  var r2 = 0;
-	  var dose = 0;
-	  var correction = 0;
 	  
-	  if(reading > 6){
-		var ISF = 100 / totalDose;
-		correction = (reading - 1) / (Math.round(ISF * 100)/100);
-		r2 = Math.round(correction * 100)/100;
-	  }
+		const dose = UserInfo.find({
+					$or: [
+							{ userId: this.userId }
+						],
+					}).map(function(post) { return post.totalDose; });
 	  
-	  if (correction > 0){
-		dose = (carb / ICR) + correction;
-	  }else{
-		dose = (carb / ICR);
-	  }
-	  var r3 = Math.round(dose * 100)/100;
+		const ICR = 450 / dose;
+		const r1 = Math.round(ICR * 100)/100;
+		let r2 = 0;
+		let curDose = 0;
+		let correction = 0;
+	  
+		if(reading > 6){
+			const ISF = 100 / dose;
+			correction = (reading - 1) / (Math.round(ISF * 100)/100);
+			r2 = Math.round(correction * 100)/100;
+		}
+	  
+		if (correction > 0){
+			curDose = (carb / ICR) + correction;
+		}else{
+			curDose = (carb / ICR);
+		}
+		const r3 = Math.round(curDose * 100)/100;
 	  
 	  return r3;
   },
