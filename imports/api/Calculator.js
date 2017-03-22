16e3ae46d9cb5a595c2807c149e64266c1d02196
Haikal Izzuddin <heywhite69@gmail.com>
2017-03-22 10:19:13 +0800

    Add BMI calculations & improve documentation

--- imports/api/Calculator.js
+++ imports/api/Calculator.js
@@ -22,17 +22,26 @@ if (Meteor.isServer) {
 }
 
 Meteor.methods({
+  // Calculate the ICR (Insulin carbohydrate ratio) for patient
+  // totalDose = total dose;
+  /* Todo: totalDose should be taken directly from database */
   'calculator.calcICR'( totalDose ) {
 		var ICR = 450 / totalDose;
 		var r = Math.round(ICR * 100)/100;
 		return r;
   },
+  // Calculate the ISF (Insulin sensitivity factor) for patient
+  // totalDose = total dose;
+  /* Todo: totalDose should be taken directly from database */
   'calculator.calcISF'( totalDose ) {
 	  var ISF = 100 / totalDose;
 	  var r = Math.round(ISF * 100)/100;
 		
 	  return r;
   },
+  // Calculate the correction for patient
+  // reading = glucose reading; totalDose = total dose;
+  /* Todo: totalDose should be taken directly from database */
   'calculator.calcCorrection'( reading, totalDose ) {
 	  var ISF = 100 / totalDose;
 	  var correction = (reading - 6) / (Math.round(ISF * 100)/100);
@@ -40,6 +49,9 @@ Meteor.methods({
 	  
 	  return r;
   },
+  // Calculate the dose of patient
+  // carb = intake carbohydrate; reading = glucose reading; totalDose = total dose;
+  /* Todo: totalDose should be taken directly from database */
   'calculation.calcDose' ( carb, reading, totalDose ) {
 	  var ICR = 450 / totalDose;
 	  var r1 = Math.round(ICR * 100)/100;
@@ -62,4 +74,27 @@ Meteor.methods({
 	  
 	  return r3;
   },
+  // Calculate the BMI of patient (This is in metric)
+  // kg = weight; htc = height;
+  'calculation.calcBMI' (kg, htc){
+	  m = htc/100;
+	  h2 = m * m;
+	  
+	  bmi = kg/h2;
+	  
+	  f_bmi = Math.floor(bmi);
+	  
+	  diff = bmi - f_bmi;
+	  diff = diff * 10;
+	  
+	  diff = Math.round(diff);
+	  if( diff == 10 ){
+		  // Need to bump up the whole thing instead
+		  f_bmi += 1;
+		  diff = 0;
+	  }
+	  bmi = f_bmi + "." + diff;
+	  
+	  return bmi;
+  }
 });
