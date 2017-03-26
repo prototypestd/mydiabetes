b5ebaaf098b81251c082c56d55b4c2bb735e2308
Haikal Izzuddin <heywhite69@gmail.com>
2017-03-26 22:39:28 +0800

    Add firstrun commands

---
+++ imports/startup/firstrun.js
@@ -0,0 +1,20 @@
+import { Meteor } from 'meteor/meteor';
+
+Meteor.startup(function () {
+	
+	/*
+	 * Creates a first user.
+	 * Useful for new instances of mydiabetes
+	 * Username: admin
+	 * Password: admin
+	 */
+	if( Meteor.users.find().count() === 0 ){
+		id = Accounts.createUser({
+			username: 'admin',
+			email: 'admin@mydiabetes',
+			password: 'admin',
+		});
+		
+		Roles.setUserRoles(id, 'super-admin', 'staff');
+	}
+});
