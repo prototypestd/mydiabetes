d449579ff459b8402e9988f1adcbd42306e1a50d
Haikal Izzuddin <heywhite69@gmail.com>
2017-04-06 05:53:13 +0000

    Temporarily remove onLogin hook

--- imports/api/Users.js
+++ imports/api/Users.js
@@ -20,6 +20,24 @@ if (Meteor.isServer) {
 			],
 		});
 	});
+
+/*	Accounts.onLogin(function() {
+		var isFirstTime = LocalStore.get('isFirstTime', {reactive: false});
+		console.log(LocalStore.get('isFirstTime', {reactive: false}));
+
+		if(isFirstTime !== true){
+			LocalStore.set('isFirstTime', true, {reactive: false});
+			
+			UserInfo.insert({
+				userId: user.user._id,
+				totalDose: 0,
+				icr: 0,
+				isf: 0
+			});
+
+			console.log(LocalStore.get('isFirstTime', {reactive: false}));
+		}
+	});*/
 	
     Meteor.publish('invites', function userInfo() {
 		return Invites.find({});
