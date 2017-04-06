4e267da4b36c266ab2fd931230b385dc89b07d62
Haikal Izzuddin <heywhite69@gmail.com>
2017-04-06 06:32:17 +0000

    firstrun.js calls the wrong method

--- server/startup/firstrun.js
+++ server/startup/firstrun.js
@@ -2,7 +2,7 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(function() {
 	if(Meteor.users.find().count() === 1){
-		Roles.addUsersToRole(Meteor.users.findOne()._id, ['super-admin'],'staff');
+		Roles.addUsersToRoles(Meteor.users.findOne()._id, ['super-admin'],'staff');
 	}
 });
 	
